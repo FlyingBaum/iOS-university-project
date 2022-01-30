1bb2f1737d99dfbdaed4677265f80d299db63142
FlyingBaum <kristgsus@gmail.com>
2022-01-30 03:18:57 +0100

    Initial Commit

---
+++ iOS6-NguyenBaliev/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS6-NguyenBaliev
+//
+//  Created by Krist Baliev on 30.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS6-NguyenBaliev/iOS6_NguyenBalievApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS6_NguyenBalievApp.swift
+//  iOS6-NguyenBaliev
+//
+//  Created by Krist Baliev on 30.01.22.
+//
+
+import SwiftUI
+
+@main
+struct iOS6_NguyenBalievApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
